4e9dd0df1d1bbd9e0f3a062ecfa1b9a104c8032e
skybaiyujing <75843499+skybaiyujing@users.noreply.github.com>
2025-05-22 17:27:32 +0800

    Add files via upload

--- service/delete.go
+++ service/delete.go
@@ -9,7 +9,15 @@ func DeleteUserByID(db *gorm.DB, id string) error {
 	result := db.Delete(&model.User{}, "id = ?", id)
 	return result.Error
 }
+
 func DeleteStudentBySno(db *gorm.DB, sno string) error {
-	result := db.Delete(&model.Student{}, "sno = ?", sno)
-	return result.Error
+	return db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Where("sno = ?", sno).Delete(&model.FamilyInfo{}).Error; err != nil {
+			return err
+		}
+		if err := tx.Delete(&model.Student{}, "sno = ?", sno).Error; err != nil {
+			return err
+		}
+		return nil
+	})
 }
